d2ebd120fdb9961b1d6209baeffe169cbf9b87b8
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2025-08-03 01:02:53 +0100

    chore: fixed  website form

--- src/Pages/Footer/footer-two.tsx
+++ src/Pages/Footer/footer-two.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="terminal-footer gradient-bg">
         <div className="footer-content">
-          <p className="terminal-prompt">israel@adenta:~$</p>
+          <p className="terminal-prompt">israel@london:~$</p>
           <p className="terminal-text">
             echo "Made with ❤️ by Israel" © {currentYear}
           </p>
